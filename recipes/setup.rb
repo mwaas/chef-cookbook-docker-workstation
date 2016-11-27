c3b581a2eae420c8273617503cb7e4c2402dcbf0
Nikhil Kumar <dpz64gs@dcwidavauto0143.epga.nam.gm.com>
2016-11-27 18:18:47 -0500

    Fixed issue with https_proxy in Dockerfile

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 file '/tmp/motd' do
-  content "Property of Everyone\n"
+  content 'Property of Everyone'
   mode '0664'
   owner 'root'
   group 'root'
